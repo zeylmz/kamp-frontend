c6b6ea5cfbca1c70243d54d884c75a8779674cbe
zeylmz <zeylmz245@gmail.com>
2021-03-10 23:16:40 +0300

    10 Mart Ders

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +10,18 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  constructor() {}
+  apiUrl = "http://localhost:57057/api/products/getall";
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response)=>{
+        this.products = response.data
+      })
+  }
 }
